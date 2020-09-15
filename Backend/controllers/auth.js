8ea4dc22737fb20c9336a7e1ab7657ee9c97db46
Chaurasia Rishav <aryan.rishav23@gmail.com>
2020-09-15 16:01:22 +0530

    done with authorisation and authentication and middlewares

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,12 +1,9 @@
 const User = require("../models/user")
 const { body, validationResult } = require('express-validator');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 
-exports.auth=(req,res)=>{
-
-   
-    res.send("Sign Out Kardiya")
-}
 
 exports.signup=(req,res) => {
 
@@ -32,4 +29,67 @@ exports.signup=(req,res) => {
         }
 
     })
+}
+
+exports.signin=(req,res) => {
+    const {email,password} = req.body
+    const errors= validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            "err":errors.array()[0].msg,
+            "error place":errors.array()[0].param
+        })
+    }
+
+    User.findOne({email:email} ,(err,user) => {
+        if(err || !user) {
+            return res.status(400).json({
+                err: "Email doesn't exist"
+            })
+        }
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                err: "Password doesn't match to the email"
+            })
+        }
+        //create token 
+        const token= jwt.sign({_id:user._id}, process.env.SECRET);
+        //put token in cookie
+        res.cookie("token", token, {expire: new Date() + 9999});
+        //send response to front end
+        const {_id,name,email,role} = user
+        return res.json({token, user:{ _id,name,email,role }});
+   });
+}
+
+exports.signout=(req,res)=>{
+    res.clearCookie("token");
+    res.send("User is signed out")
+}
+
+// protected route
+exports.isSignedIn = expressJwt({
+    secret:process.env.SECRET,
+    userProperty:"auth"
+})
+
+//custom middleware
+exports.isAuthenticated=(req,res,next) => {
+    let checker= req.profile && req.auth && req.profile._id === req.auth._id
+    if(!checker){
+        return res.status(403).json({
+            error:"ACCESS DENIED"
+        })
+    }
+    next()
+}
+
+exports.isAdmin=(req,res,next) => {
+    if(role===0){
+        return res.status(403).json({
+            error:"you are not admin, ACCESS DENIED"
+        })
+    }
+    next()
 }
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,7 +1,7 @@
 let express= require("express");
 let router=express.Router();
 const { body, validationResult } = require('express-validator');
-let {auth,signup}=require("../controllers/auth")
+let {signout,signup,signin,isSignedIn}=require("../controllers/auth")
 
 
 router.post("/signup",[
@@ -9,8 +9,15 @@ router.post("/signup",[
     body('password').isLength({ min: 5 }).withMessage(' Password must be at least 5 chars long'),
     body('name').isLength({ min: 3 }).withMessage(' Name must be at least 3 chars long')
 ],signup)
-router.get("/signout",auth)
+router.get("/signout",signout)
 
+router.post("/signin",[
+    body('email').isEmail().withMessage('Enter A Email'),
+    body('password').isLength({ min: 1 }).withMessage(' Password should not be empty'),
+],signin)
+
+router.get("/testroute",isSignedIn,(req,res) => {
+    res.send("hi i got ")
+})
 
 module.exports= router;
-// Changed//
